7dbb014beaaf88a5b63d089e78cccbdb9e785208
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-06-14 11:43:07 +0200

    [FIX] errors

--- application/views/index.php
+++ application/views/index.php
@@ -26,14 +26,16 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 	<!-- //Meta tag Keywords -->
 
 	<!-- Custom-Files -->
-	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/bootstrap.css"> -->
 	<!-- Bootstrap-Core-CSS -->
-	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	<!-- <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/style.css" type="text/css" media="all" /> -->
 	<!-- Style-CSS -->
-	<link href="css/font-awesome.min.css" rel="stylesheet">
+	<!-- <link href="<?= base_url()?>/css/font-awesome.min.css" rel="stylesheet"> -->
 	<!-- Font-Awesome-Icons-CSS -->
 	<!-- //Custom-Files -->
-
+	<?php 
+		echo base_url();
+	?>
 	<!-- Web-Fonts -->
 	<link href="//fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=devanagari,latin-ext"
 	 rel="stylesheet">
