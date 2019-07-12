37e3747168994a48b2d5b2267029e60f45467439
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-07-12 11:15:41 +0200

    [UPDATE] recuperation des donnees

--- application/controllers/Article.php
+++ application/controllers/Article.php
@@ -5,7 +5,10 @@
         public function set_artcle(){
             $type = $this->input->post('type');
             $mention = $this->input->post('mention');
-            
+            $date = $this->input->post('mention');
+            $id_client = $this->input->get('id_client');
+            $vue = 89;
+
             $data = array(
 
             );
