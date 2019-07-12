99abfb2782037196e3d61d4fba130e93e7ad3bb6
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-07-12 11:13:18 +0200

    [ADD] ajout de la methode set_article

--- application/controllers/Article.php
+++ application/controllers/Article.php
@@ -3,6 +3,8 @@
     class Article extends CI_Controller{
         
         public function set_artcle(){
+            $type = $this->input->post('type');
+            $mention = $this->input->post('mention');
             
             $data = array(
 
