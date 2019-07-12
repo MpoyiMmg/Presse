e33daee47acb8bf011f008fc27faff73462eb0e2
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-07-12 10:53:10 +0200

    [ADD] creation de la methode get_comment

--- application/models/Commentaire.php
+++ application/models/Commentaire.php
@@ -5,6 +5,13 @@
         public function set_comment($data){
             $this->db->insert('coommentaire', $data);
         }
+
+        public function get_comment($id_comment){
+            $query = $this->db->where('id', $id_comment)
+                     ->get('commentaire')
+                     ->result();
+            return $query;
+        }
     }
 
 ?>
