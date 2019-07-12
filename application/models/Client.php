f1fb4ce884c32d9334e4bd5838b1bd14f8bc9e32
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-07-12 10:58:23 +0200

    [ADD] creation de la methode get_client

--- application/models/Client.php
+++ application/models/Client.php
@@ -1,7 +1,18 @@
 <?php 
 
     class Client extends CI_Model{
-        
+
+        public function set_client($data){
+            $this->db->insert('client', $data);
+        }
+
+        public function get_client($id_client){
+            $query = $this->db->where('id', $id_client)
+                              ->get('client')
+                              ->result();
+        }
+
+        public 
     }
 
 ?>
