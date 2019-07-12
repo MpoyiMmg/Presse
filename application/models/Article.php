4d031f0ec3891c05a0568f8e3004cb11a53d0007
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-07-12 10:44:22 +0200

    [ADD] creation de la methode set_commentaire

--- application/models/Article.php
+++ application/models/Article.php
@@ -2,14 +2,20 @@
 
     class Article extends CI_Model{
         
+        #############################################
         public function set_article($data){
             $this->db->insert('article', $data);
         }
-
+        #############################################
         public function get_article(){
             $query = $this->db->get('article')
                               ->result();
             return $query;
+        }
+        #############################################
+        public function set_commentaire($id_article, $data){
+            $this->db->where('id', $id_article)
+                     ->insert('commentaire', $data);
         }
     }
 ?>
